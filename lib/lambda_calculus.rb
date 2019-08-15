982d6cd669c9bcdf99c1d8bd04484ccf7b221b37
Pavel Kotlyar <pavel.kotlyar@icloud.com>
2019-08-16 00:48:25 +1000

    refactor to use hash instead of OpenStruct to store digits info

--- lib/lambda_calculus.rb
+++ lib/lambda_calculus.rb
@@ -1,28 +1,16 @@
 require "lambda_calculus/version"
-require "ostruct"
 
 module LambdaCalculus
-  DIGITS = [
-    OpenStruct.new(name: :zero, value: 0),
-    OpenStruct.new(name: :one, value: 1),
-    OpenStruct.new(name: :two, value: 2),
-    OpenStruct.new(name: :three, value: 3),
-    OpenStruct.new(name: :four, value: 4),
-    OpenStruct.new(name: :five, value: 5),
-    OpenStruct.new(name: :six, value: 6),
-    OpenStruct.new(name: :seven, value: 7),
-    OpenStruct.new(name: :eight, value: 8),
-    OpenStruct.new(name: :nine, value: 9)
-  ].freeze
+  DIGITS = { zero: 0, one: 1, two: 2, three: 3, four: 4, five: 5, six: 6, seven: 7, eight: 8, nine: 9 }.freeze
 
   # generates methods like this for all supported digits
   # def zero(operation = nil)
   #   operation ? operation.call(0) : 0
   # end
   # etc...
-  DIGITS.each do |digit|
-    define_method digit.name do |operation = nil|
-      operation ? operation.call(digit.value) : digit.value
+  DIGITS.each do |name, value|
+    define_method name do |operation = nil|
+      operation ? operation.call(value) : value
     end
   end
 
