c3a647ec36226c0a32575c475f109f491bd38cfb
Pavel Kotlyar <pavel.kotlyar@icloud.com>
2019-08-15 15:32:30 +1000

    add multiplying 6 by 5 initial spec

--- lib/lambda_calculus.rb
+++ lib/lambda_calculus.rb
@@ -1,6 +1,15 @@
 require "lambda_calculus/version"
 
 module LambdaCalculus
-  class Error < StandardError; end
-  # Your code goes here...
+  def self.five(operation = nil)
+
+  end
+
+  def self.six(operation = nil)
+
+  end
+
+  def self.times(number)
+
+  end
 end
--- spec/lambda_calculus_spec.rb
+++ spec/lambda_calculus_spec.rb
@@ -1,9 +1,9 @@
 RSpec.describe LambdaCalculus do
-  it "has a version number" do
+  it 'has a version number' do
     expect(LambdaCalculus::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it 'multiplying 6 by 5' do
+    expect(LambdaCalculus.six(LambdaCalculus.times(LambdaCalculus.five))).to eq(30)
   end
 end
