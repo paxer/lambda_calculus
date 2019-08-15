512373eb2233b68b95b4d1bf4b8b1c32d3beb334
Pavel Kotlyar <pavel.kotlyar@icloud.com>
2019-08-15 15:21:27 +1000

    init commit

---
+++ lib/lambda_calculus.rb
@@ -0,0 +1,6 @@
+require "lambda_calculus/version"
+
+module LambdaCalculus
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/lambda_calculus/version.rb
@@ -0,0 +1,3 @@
+module LambdaCalculus
+  VERSION = "0.1.0"
+end
